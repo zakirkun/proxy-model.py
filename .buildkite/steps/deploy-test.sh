bec96bf7e2af06f9ad08f3939c8c8e1df6166044
Ivan Loboda <lobodatell@yandex.ru>
2022-03-22 18:50:29 +0300

    Disable uniswap tests

--- .buildkite/steps/deploy-test.sh
+++ .buildkite/steps/deploy-test.sh
@@ -87,12 +87,12 @@ docker run --rm -ti --network=container:proxy \
      ${EXTRA_ARGS:-} \
      $PROXY_IMAGE \
 
-echo "Run uniswap-v2-core tests..."
-docker run --rm -ti --network=host \
-     --entrypoint ./deploy-test.sh \
-     ${EXTRA_ARGS:-} \
-     $UNISWAP_V2_CORE_IMAGE \
-     all
+#echo "Run uniswap-v2-core tests..."
+#docker run --rm -ti --network=host \
+#     --entrypoint ./deploy-test.sh \
+#     ${EXTRA_ARGS:-} \
+#     $UNISWAP_V2_CORE_IMAGE \
+#     all
 
 echo "Run tests return"
 exit 0
